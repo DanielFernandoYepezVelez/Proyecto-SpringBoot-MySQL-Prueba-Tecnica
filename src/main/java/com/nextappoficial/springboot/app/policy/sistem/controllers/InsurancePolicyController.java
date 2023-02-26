b398bdd4e795c3ef878f552d90cd8ce157bb06ef
Daniel Fernando Yepez Velez <dfyepes@personalsoft.com.co>
2023-02-26 16:34:11 -0500

    Solucionando El Problema De Las Polizas

--- src/main/java/com/nextappoficial/springboot/app/policy/sistem/controllers/InsurancePolicyController.java
+++ src/main/java/com/nextappoficial/springboot/app/policy/sistem/controllers/InsurancePolicyController.java
@@ -5,6 +5,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import com.nextappoficial.springboot.app.policy.sistem.models.entity.Car;
+import com.nextappoficial.springboot.app.policy.sistem.models.entity.Client;
+import com.nextappoficial.springboot.app.policy.sistem.models.services.ICarService;
+import com.nextappoficial.springboot.app.policy.sistem.models.services.IClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
@@ -28,6 +32,12 @@ public class InsurancePolicyController {
 	
 	@Autowired
 	private IInsurancePolicyService insurancePolicyService;
+
+	@Autowired
+	private IClientService clientService;
+
+	@Autowired
+	private ICarService carService;
 	
 	@GetMapping("/policies")
 	public List<InsurancePolicy> getPolicies() {
@@ -56,11 +66,44 @@ public class InsurancePolicyController {
 		return new ResponseEntity<InsurancePolicy>(insurancePolicy, HttpStatus.OK);
 	}
 	
-	@PostMapping("/policy")
-	public ResponseEntity<?> create(@Valid @RequestBody InsurancePolicy policy, BindingResult result) {
+	@PostMapping("/policy/{dni}/{registration_number}")
+	public ResponseEntity<?> create(@Valid @RequestBody InsurancePolicy policy, BindingResult result,
+									@PathVariable String dni, @PathVariable String registration_number) {
+
+		Car carDB = null;
+		Client clientDB = null;
 		InsurancePolicy policyDB = null;
 		Map<String, Object> response = new HashMap<String, Object>();
-		
+
+		// En Caso Tal De Que Tengamos Un Error En La Base De Datos, Lo Manejamos Con El Try - Catch
+		try {
+			clientDB = clientService.findById(dni);
+		} catch(DataAccessException e) {
+			response.put("message", "Error Al Realizar La Consulta En La Base De Datos");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		if(clientDB == null) {
+			response.put("message", "Error: El Cliente Con El ID: ".concat(dni.toString().concat(" No Existe En El Sistema")));
+			return new ResponseEntity<Map<String, Object>>(response,HttpStatus.NOT_FOUND);
+		}
+
+
+		// En Caso Tal De Que Tengamos Un Error En La Base De Datos, Lo Manejamos Con El Try - Catch
+		try {
+			carDB = carService.findById(registration_number);
+		} catch(DataAccessException e) {
+			response.put("message", "Error Al Realizar La Consulta En La Base De Datos");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		if(carDB == null) {
+			response.put("message", "Error: El Vehículo Con El ID: ".concat(registration_number.toString().concat(" No Existe En El Sistema")));
+			return new ResponseEntity<Map<String, Object>>(response,HttpStatus.NOT_FOUND);
+		}
+
 		if(result.hasErrors()) {
 			/* ============ PROGRAMACIÓN FUNCIONAL (STREAMS) ============== */
 			List<String> errors = result.getFieldErrors()
@@ -74,7 +117,10 @@ public class InsurancePolicyController {
 		
 		// En Caso Tal De Que Tengamos Un Error En La Base De Datos, Lo Manejamos Con El Try - Catch
 		try {
+			policy.setClient(clientDB);
+			policy.setCar(carDB);
 			policyDB = insurancePolicyService.save(policy);
+
 		} catch(DataAccessException e) {
 			response.put("message", "Error Al Realizar El Insert En La Base De Datos");
 			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
